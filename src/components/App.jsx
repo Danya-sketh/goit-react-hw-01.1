f549014980094bf9825b44d2c8db692166613e9e
Danya-sketh <yannitc7@gmail.com>
2024-06-28 19:40:54 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import user from '../../user.json';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        image={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
+
+
+export default App
