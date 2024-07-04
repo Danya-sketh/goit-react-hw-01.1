8c1b3b48d7a28778aa95dabf65b1252cd7e70f6a
Danya-sketh <yannitc7@gmail.com>
2024-07-04 15:26:10 +0300

    work with app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,49 @@
-import Profile from './Profile';
-import FriendList from './FriendList';
-import FriendListItem from './FriendListItem';
-import TransactionHistory from './TransactionHistory';
-import Container from './Container';
-import userInfo from '../user.json';
+// import Profile from './Profile';
+// import FriendList from './FriendList';
+// import FriendListItem from './FriendListItem';
+// import TransactionHistory from './TransactionHistory';
+// import Container from './Container';
+// import userInfo from '../user.json';
+// import friends from '../friends.json';
+// import transactions from '../transactions.json';
+
+// function App() {
+//   return (
+  
+// <Container>
+//       <Profile
+//         name={user.name}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//       <Statistics stats={statisticalData} />
+//       <FriendList>
+//         {friends.map(friend => (
+//           <FriendListItem
+//             key={friend.id}
+//             avatar={friend.avatar}
+//             name={friend.name}
+//             isOnline={friend.isOnline}
+//             id={friend.id}
+//           />
+//         ))}
+//       </FriendList>
+//       <TransactionHistory items={transactions} />
+//     </Container>
+//   )};
+
+
+// export default App
+
+import Container from './Container'
+import Profile from './Profile/Profile'
+import FriendList from './FriendList'
+import FriendListItem from './FriendListItem'
+import TransactionHistory from './TransactionHistory'
+
+import user from '../user.json';
 import friends from '../friends.json';
 import transactions from '../transactions.json';
 
@@ -18,7 +58,7 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics stats={statisticalData} />
+    
       <FriendList>
         {friends.map(friend => (
           <FriendListItem
@@ -34,5 +74,3 @@ function App() {
     </Container>
   )};
 
-
-export default App
